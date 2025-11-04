e3879c83dab732d0738813fd219846a64c25a92c
Kaverichillamcharla <kaverichillamcharla2003@gmail.com>
2025-11-04 16:50:18 +0530

    Update
    Refactor navigation structure and improve styling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import LoginForm from "./LoginForm";
 import RegisterForm from "./RegisterForm";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <Link to="/register">Register</Link> | <Link to="/login">Login</Link>
-      </nav>
+      <div className="navbar">
+        <h2 className="brand">SSO Application</h2>
+        <div>
+          <Link to="/register" className="nav-link">Register</Link>
+          <Link to="/login" className="nav-link">Login</Link>
+        </div>
+      </div>
+
       <Routes>
         <Route path="/register" element={<RegisterForm />} />
         <Route path="/login" element={<LoginForm />} />
